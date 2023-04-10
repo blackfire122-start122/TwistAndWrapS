431f351adaca2afd228143fcff514a118e839760
igor <gsambir519@gmail.com>
2023-04-10 18:01:11 +0300

    add cors middelware

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// in deploy need be set gloabal ip
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	internal.SetRouters(router)
 	router.Run("localhost:8080")
 }
