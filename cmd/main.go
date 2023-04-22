9b98b27745770613ac764320009158b26cf83377
igor <gsambir519@gmail.com>
2023-04-22 08:50:01 +0300

    api links for nginx and delete cors middlware

--- cmd/main.go
+++ cmd/main.go
@@ -2,30 +2,16 @@ package main
 
 import (
 	"TwistAndWrapS/internal"
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// in deploy need be set gloabal ip
-		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-
 func main() {
 	router := gin.Default()
-	router.Use(CORSMiddleware())
 	internal.SetRouters(router)
 	go internal.Broadcaster()
-	router.Run("localhost:8080")
+	err := router.Run("localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
